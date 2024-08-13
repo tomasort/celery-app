06175cda63b2a79ca16fd9272e10cbf4a1e3108d
tomasort <tomasort@gmail.com>
2024-08-13 16:46:38 -0400

    fixed the error handlers

--- flask-app/app/__init__.py
+++ flask-app/app/__init__.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask
+from flask import Flask, render_template
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from logging.handlers import RotatingFileHandler
