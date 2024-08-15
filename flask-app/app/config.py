ee9f8145117e882f97d5c63a470ff849385126b8
tomasort <tomasort@gmail.com>
2024-08-14 22:47:40 -0400

    added broker_connection_retry_on_startup to the celery configuration to avoid a warning

--- flask-app/app/config.py
+++ flask-app/app/config.py
@@ -13,6 +13,7 @@ class BaseConfig:
     CELERY=dict(
         broker_url=os.environ.get("BROKER_URL", "redis://127.0.0.1:6379/0"),
         result_backend=os.environ.get("RESULT_BACKEND", "redis://127.0.0.1:6379/0"),
+        broker_connection_retry_on_startup=True,
     )
 
 class DevelopmentConfig(BaseConfig):
